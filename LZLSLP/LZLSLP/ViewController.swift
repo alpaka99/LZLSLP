a1ef8fdd692a5bf86243c5967d6a94711173ecc5
Lets <ruvan123@naver.com>
2024-08-15 16:13:58 +0900

    Feat: 프로젝트 생성

---
+++ LZLSLP/LZLSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LZLSLP
+//
+//  Created by user on 8/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
