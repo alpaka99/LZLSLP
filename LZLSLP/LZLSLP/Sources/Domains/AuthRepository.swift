e48a3cfae2e436f97ffdc10deefb254cfc1e7629
Lets <ruvan123@naver.com>
2024-08-17 21:31:43 +0900

    Feat: SignupViewController Dummy UI와 AuthRepository 구현

--- LZLSLP/LZLSLP/Sources/Domains/AuthRepository.swift
+++ LZLSLP/LZLSLP/Sources/Domains/AuthRepository.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+import RxSwift
+
 final class AuthRepository {
+    let disposeBag = DisposeBag()
     
+    var router = PublishSubject<Router>()
+    var data = PublishSubject<Data>()
+    
+    init() {
+        configureBind()
+    }
+    
+    func configureBind() {
+        router
+            .bind(with: self) { owner, router in
+                let result = NetworkManager.shared.requestCall(router: router)
+                    .catch { error in
+                        Single.just(Data())
+                    }
+                
+                
+            }
+            .disposed(by: disposeBag)
+    }
 }
--- LZLSLP/LZLSLP/Sources/Presenters/Signup/SignupView.swift
+++ LZLSLP/LZLSLP/Sources/Presenters/Signup/SignupView.swift
@@ -7,6 +7,91 @@
 
 import UIKit
 
+import SnapKit
+
 final class SignupView: BaseView {
+    let emailTextField = {
+        let textField = UITextField()
+        textField.placeholder = "이메일을 입력해주세요"
+        return textField
+    }()
     
+    let passwordTextField = {
+        let textField = UITextField()
+        textField.placeholder = "비밀번호를 입력해주세요"
+        return textField
+    }()
+    
+    let nicknameTextField = {
+        let textField = UITextField()
+        textField.placeholder = "닉네임을 입력해주세요"
+        return textField
+    }()
+    
+    let phoneNumTextField = {
+        let textField = UITextField()
+        textField.placeholder = "전화번호를 입력해주세요"
+        return textField
+    }()
+    
+    lazy var textFieldStack = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.addArrangedSubviews([
+            emailTextField,
+            passwordTextField,
+            nicknameTextField,
+            phoneNumTextField,
+            birthDayTextField
+        ])
+        
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    let birthDayTextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    let submitButton = {
+        let button = UIButton.Configuration.plain()
+            .backgroundColor(.systemBlue)
+            .title("회원 가입")
+            .cornerStyle(.capsule)
+            .build()
+        
+        button.tintColor = .white
+        return button
+    }()
+    
+    
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        self.addSubview(textFieldStack)
+        self.addSubview(submitButton)
+    }
+    
+    override func configureLayout() {
+        super.configureLayout()
+        
+        textFieldStack.snp.makeConstraints { stack in
+            stack.width.equalTo(self.safeAreaLayoutGuide.snp.width)
+                .multipliedBy(0.8)
+            stack.height.equalTo(self.safeAreaLayoutGuide.snp.height)
+                .multipliedBy(0.6)
+            stack.center.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        submitButton.snp.makeConstraints { btn in
+            btn.top.equalTo(textFieldStack.snp.bottom)
+                .offset(20)
+            btn.horizontalEdges.equalTo(textFieldStack.snp.horizontalEdges)
+            btn.height.equalTo(44)
+        }
+    }
 }
