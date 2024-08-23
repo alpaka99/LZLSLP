5f43f93524fccb534e8022922f0d45b50817ca40
Lets <ruvan123@naver.com>
2024-08-23 22:48:01 +0900

    Feat: Data Upload 시도중

--- LZLSLP/LZLSLP/Sources/Utils/Managers/NetworkManager.swift
+++ LZLSLP/LZLSLP/Sources/Utils/Managers/NetworkManager.swift
@@ -14,7 +14,7 @@ final class NetworkManager {
     static let shared = NetworkManager()
     private init() { }
     
-    func requestCall(router: Router, interceptor: AuthInterceptor? = nil) -> Single<Result<Data, Error>> {
+    func requestCall(router: Router, interceptor: Interceptor? = nil) -> Single<Result<Data, Error>> {
         Single.create { observer in
             if let urlRequest = router.build() {
                 AF.request(urlRequest, interceptor: interceptor)
@@ -50,46 +50,54 @@ final class NetworkManager {
             return Disposables.create()
         }
     }
+    
+    func multipartFormRequest(router: URLRouter, data: [Data], interceptor: Interceptor? = nil) -> Single<Result<Data, Error>> {
+        Single.create { observer in
+            if let urlRequest = router.build() {
+                
+                // files라는 parameter 이름으로 이미지 파일들을 올려버림
+                AF.upload(multipartFormData: { multipartFormData in
+                    
+                }, with: urlRequest)
+//                AF.request(urlRequest, interceptor: interceptor)
+//                    .validate(statusCode: 200..<300)
+//                    .responseString { result in
+//                        switch result.result {
+//                        case .success(let str):
+//                            print(str)
+//                        case .failure(let error):
+//                            print(error)
+//                        }
+//                    }
+//                    .responseData { result in
+//                        switch result.result {
+//                        case .success(let data):
+//                            observer(.success(.success(data)))
+//                        case .failure(let error):
+//                            print("NetworkManager error: \(error)")
+//                            switch error {
+//                            case .createURLRequestFailed:
+//                                observer(.failure(NetworkError.urlRequestCreateError))
+//                            case .responseValidationFailed:
+//                                observer(.failure(NetworkError.responseStatusCodeError))
+//                            default:
+//                                observer(.failure(NetworkError.networkError))
+//                            }
+//                        }
+//                    }
+            } else {
+                observer(.failure(NetworkError.urlRequestCreateError))
+            }
+            
+            return Disposables.create()
+        }
+    }
 }
 
 struct RefreshTokenResponse: Decodable {
     let accessToken: String
 }
 
-//final class Interceptor {
-//    /*
-//     MARK: Interceptor Logic 만들기
-//     
-//     Interceptor:
-//     */
-//    func intercept(completionHandler: @escaping (Result<Data, Error>)->Void) {
-//        if let urlRequest = URLRouter.https(.lslp(.auth(.accessToken))).build() {
-//            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-//                guard error == nil else {
-//                    completionHandler(.failure(InterceptorError.tokenFetchError))
-//                    return
-//                }
-//                
-//                guard let response = response as? HTTPURLResponse, (200..<300).contains(response.statusCode) else {
-//                    completionHandler(.failure(InterceptorError.tokenResponseStatusError))
-//                    return
-//                }
-//                
-//                guard let data = data else {
-//                    completionHandler(.failure(InterceptorError.nilTokenError))
-//                    return
-//                }
-//                
-//                
-//                completionHandler(.success(data))
-//                return
-//            }
-//            .resume()
-//        } else {
-//            completionHandler(.failure(InterceptorError.tokenURLBuildError))
-//        }
-//    }
-//}
 
 enum NetworkError: Error {
     case urlRequestCreateError
